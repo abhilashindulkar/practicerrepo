0f7c6090cbaa41e212ee129f2f30823639ba356f
Abhilash Indulkar <abhilashindulkar@gmail.com>
2020-06-14 20:47:37 +0530

    added comments

--- practicerepo/src/practicejava/DummyClass.java
+++ practicerepo/src/practicejava/DummyClass.java
@@ -3,5 +3,9 @@ package practicejava;
 public class DummyClass {
 	public static void main(String[] args) {
 		System.out.println("hello from the eclipse");
+		//added single line comment
+		/* added multi line comment
+		 * 
+		 */
 	}
 }
